40c70f3eb407002f2833140bfa32a64613b810c0
yanchen <yanchen@yanchentekiMacBook-puro.local>
2019-10-07 14:10:09 +0200

    simple inspector

--- Mini Game/Assets/Player.cs
+++ Mini Game/Assets/Player.cs
@@ -9,10 +9,15 @@ public class Player : MonoBehaviour
     public FloatVariable rotationSpeed;
     public Vector3Variable playerPosition;
 
+    [HideInInspector]
+    public float movespeed;
+    [HideInInspector]
+    public float rotationspeed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        moveSpeed.Value = movespeed;
+        rotationSpeed.Value = movespeed;
     }
 
     // Update is called once per frame
@@ -25,5 +30,7 @@ public class Player : MonoBehaviour
 
 
     }
+
+    
     
 }
