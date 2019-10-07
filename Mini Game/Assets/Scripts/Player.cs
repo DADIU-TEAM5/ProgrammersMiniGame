b0ae8c0903ff2c8ca3b7e8ed9aa7f876d78c9d63
Magnus <magnusmhp@gmail.com>
2019-10-07 15:27:04 +0200

    now you can die in peace

--- Mini Game/Assets/Scripts/Player.cs
+++ Mini Game/Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -40,7 +41,8 @@ public class Player : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
-        
+        if(collision.gameObject.CompareTag("Enemy"))
+        SceneManager.LoadScene("DeadScene");
     }
 
 
