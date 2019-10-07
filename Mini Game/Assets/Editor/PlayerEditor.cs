0b598b233a106859d77690623cb7dd80b12f1d0d
yanchen <yanchen@yanchentekiMacBook-puro.local>
2019-10-07 13:15:58 +0200

    editor

---
+++ Mini Game/Assets/Editor/PlayerEditor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TestEditor))]
+public class PlayerEditor : Editor
+{
+    private TestEditor testEditor;
+
+    private bool playerInfoIsOpen = true;
+    private bool playerStatsIsOpen = false;
+
+    private void OnEnable()
+    {
+        testEditor = (TestEditor)target;
+        if(testEditor.playerStats == null)
+        {
+            testEditor.playerStats = "test";
+        }
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+    }
+}
