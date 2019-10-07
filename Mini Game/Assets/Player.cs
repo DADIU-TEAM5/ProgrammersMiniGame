ed1488506a93ba794b2bb066ecdfd6bd2f6ac0b4
Magnus <magnusmhp@gmail.com>
2019-10-07 13:15:36 +0200

    the standard set up

---
+++ Mini Game/Assets/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    CharacterController controller;
+    public float moveSpeed = 10;
+    public float rotationSpeed = 10;
+    Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed);
+        transform.Rotate(Vector3.up, Time.deltaTime * Input.GetAxis("Horizontal") * rotationSpeed);
+    }
+    
+}
