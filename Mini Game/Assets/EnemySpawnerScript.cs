22a8658e80b0210cf7f6bebf1d556cb993902949
Marlleh <martino1215@hotmail.com>
2019-10-07 15:51:20 +0200

    All the EnemySpawner stuff

---
+++ Mini Game/Assets/EnemySpawnerScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnerScript : MonoBehaviour
+{
+    public EnemySpawner enemySpawner;
+    public Vector2Variable planeSize;
+    private enemyController ec;
+    private float spawnTime = 0;
+
+
+    void Update()
+    {
+        spawnTime += Time.deltaTime;
+
+        if (spawnTime > enemySpawner.SpawnDelay)
+        {
+            SpawnEnemies();
+            spawnTime = 0;
+
+        }
+    }
+
+    void SpawnEnemies()
+    {
+        for (int i = 0; i < enemySpawner.EnemiesSpawned; i++)
+        {
+            float randomX = Random.Range(-planeSize.Value.x*0.1f, planeSize.Value.x * 0.1f);
+            float randomZ = Random.Range(-planeSize.Value.y * 0.1f, planeSize.Value.y * 0.1f);
+
+            Vector3 temp = new Vector3(randomX, 50, randomZ);
+            Quaternion tempQ = new Quaternion(0, 0, 0, 0);
+
+            GameObject newEnemy = GameObject.Instantiate(enemySpawner.enemy, temp, tempQ);
+            ec = newEnemy.GetComponent<enemyController>();
+            ec.BehaviourScript = enemySpawner.behaviourScript;
+
+        }
+    }
+
+
+}
---
+++ Mini Game/Assets/PlaneController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour
+{
+    public Vector2Variable planeScale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 size = new Vector3(planeScale.Value.x* 0.1F,100 , planeScale.Value.y * 0.1F);
+        transform.localScale = size;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
