e29aeda9e5f9390054f17b38aa3c9f302b738b7f
Marlleh <martino1215@hotmail.com>
2019-10-07 13:29:33 +0200

    EnemyBehaviour

---
+++ Mini Game/Assets/Scripts/ScriptableObjectsScripts/EnemyBehaviour.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EnemyBehaviour : MonoBehaviour
+{
+    public abstract void doBehaviour();
+ 
+}
